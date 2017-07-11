f1642c8be2b1264f4bbdeb5bf90c88c2408e0284
nolialsea <nolialsea@hotmail.fr>
2017-07-11 16:15:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
 /*
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
-BotCoin !
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
-IRC bot that allows people to mine virtual, worthless gold, then use it (craft), trade it (for crafted stuff) or gamble it (because).
-Players can create accounts and log in with PM command, they stay connected even when they change nick (store realname to stay connected ?)
-Each player have a pickaxe they can use every X minute(s).
-
-TODO :
-    - Mining is scaled by delta (Max 1G/d ?)
-
-POSSIBLE FUTURES :
-	- Mining zones ! Mine the channel or BotCoin himself and get different outcomes
-	- Pickaxes ! Craftable, upgradable(at some extent?), breakable, repairable(?)
-	- TAX UPDATE ???
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 */
 
