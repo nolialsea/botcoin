b6d008fa80ee3efa3d898ed4057121ae24a92c9c
nolialsea <nolialsea@hotmail.fr>
2017-07-18 17:56:48 +0200

    Small fixes

--- server.js
+++ server.js
@@ -1,1 +1,4 @@
 const main = require('main');
+//Yeah, nothing else.
+//Nodejs modules are singleton if you import them from the same location
+//So it's just an ugly way to avoid some problems with database and irc client
